b5651bcb1e5a579c7beaf98c74fa06cae5ebd05c
annienouddavanh <139666145+annienouddavanh@users.noreply.github.com>
2023-07-16 15:25:47 +0700

    add icon

---
+++ AnieProject/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="d-flex flex-row mb-3">
+       
+        <div lang="la" class="p-2">ແອ່ນນີ້</div>
+        <div lang="la" class="p-2">ແອ່ນນີ້</div> 
+        <div class="p-2"><span class="material-symbols-outlined">
+                 home
+                  </span>
+                 <span class="material-symbols-outlined">
+                 star_rate
+                 <span class="material-symbols-outlined">
+                 deceased
+                 <div class="p-2"><span class="material-symbols-outlined">
+                 menu
+</span></div>
+        
+                 </span>
+            </span></div>
+    </div>
+    <div class="d-flex flex-row-reverse">
+        <div class="p-2">Flex item 1</div>
+        <div class="p-2">Flex item 2</div>
+        <div class="p-2">Flex item 3</div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
